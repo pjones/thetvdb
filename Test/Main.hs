28953714aa92bbfa47c1a92b74c10e9651425935
Peter Jones <pjones@pmade.com>
2013-02-05 12:24:45 -0700

    Write some initial tests

--- Test/Main.hs
+++ Test/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 {-
 
 This file is part of the Haskell package thetvdb. It is subject to the
@@ -10,12 +12,45 @@ the LICENSE file.
 -}
 
 module Main where
-import Network.API.TheTVDB.API
+import Control.Monad (liftM, when)
+import Control.Monad.Trans.Resource (runResourceT)
+import Data.Conduit (($$))
+import Data.Conduit.Binary (sourceFile)
+import Network.API.TheTVDB.Search (searchErr)
+import Network.API.TheTVDB.Types.API (API(..))
+import Network.API.TheTVDB.Types.Series (Series(..))
+import System.Exit (exitFailure)
+import Test.HUnit (Test(..), Counts(..), runTestTT,
+                   assertFailure, assertEqual, assertBool)
 
-data FromDisk = FromDisk
+-- ============================================================================
+newtype TestXML = TestXML {xmlFilePath :: FilePath}
 
-instance API FromDisk where
-  fetch = diskFetch
+instance API TestXML where
+  fetch a _ d = liftM Right $ runResourceT $ sourceFile (xmlFilePath a) $$ d
 
-diskFetch :: Query query => api -> query -> Disposition r -> IO (Result r)
-diskFetch _ q d = undefined
+-- ============================================================================
+goodSearchTest :: Test
+goodSearchTest = TestCase $ do
+  result <- searchErr (TestXML "Test/search.xml") "mother"
+  case result of
+    Left e  -> assertFailure $ show e
+    Right ss -> do
+      assertEqual "results size" 13 (length ss)
+      let s = head ss
+      assertEqual "first series ID" 178331 $ seriesID s
+      assertEqual "first series name" "Mother" $ seriesName s
+      assertEqual "first series IMDB" "tt1632065" $ seriesIMDB s
+
+-- ============================================================================
+-- Why can't this be automatic?
+unitTests :: Test
+unitTests = TestList
+  [ TestLabel "Can parse search results" goodSearchTest
+  ]
+
+-- ============================================================================
+main :: IO ()
+main = do counts <- runTestTT unitTests
+          let bad = errors counts + failures counts
+          when (bad > 0) exitFailure
