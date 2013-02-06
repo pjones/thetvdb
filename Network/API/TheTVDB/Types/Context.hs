a345f50ae6b8230a7de00ce7f44e734c46e52a8f
Peter Jones <pjones@pmade.com>
2013-02-06 15:46:20 -0700

    Integrate the HTTP code into Context and add a timeout

--- Network/API/TheTVDB/Types/Context.hs
+++ Network/API/TheTVDB/Types/Context.hs
@@ -13,20 +13,39 @@ the LICENSE file.
 module Network.API.TheTVDB.Types.Context
        (Context(..), defaultContext, posterBaseURL) where
 
-import Network.API.TheTVDB.HTTP
 import Network.API.TheTVDB.Types.API
 import Network.HTTP.Types (renderQuery, simpleQueryToQuery)
 import qualified Data.ByteString.Char8 as B
-import Network.HTTP.Conduit (Manager)
 import System.Directory (getAppUserDataDirectory)
 import qualified Data.Text as T
+import qualified Network.HTTP.Conduit as H
+import qualified Network.HTTP.Types as HS
+import qualified Data.Conduit as C
+import Control.Monad (liftM)
+import Control.Monad.Trans.Resource (ResourceT, runResourceT)
 
 -- | FIXME:
 data Context = Context
-  { apiKey      :: Key           -- ^ TheTVDB API key.
-  , apiLang     :: Language      -- ^ The metadata language to request.
-  , cacheDir    :: FilePath      -- ^ Directory to store cache files in.
-  , httpManager :: Maybe Manager -- ^ Optional 'Manager' for HTTP connections.
+  { apiKey :: Key
+    -- ^ TheTVDB API key.
+
+  , apiLang :: Language
+    -- ^ The language to request from the API.  The default is English
+    -- (en).  The language parameter is given directly to TheTVDB API
+    -- which should then return text (e.g. names and overviews) in the
+    -- requested language.
+
+  , cacheDir :: FilePath
+    -- ^ Directory to store cache files in.  Not currently used.
+
+  , httpTimeout :: Maybe Int
+    -- ^ Optional request timeout in microseconds.  This is passed to
+    -- the @http-conduit@ package.  The default is 60 seconds (the API
+    -- can be really, really slow).  'Nothing' means disable the
+    -- timeout and wait forever.
+
+  , httpManager :: Maybe H.Manager
+    -- ^ Optional 'Manager' for HTTP connections.
   }
 
 instance API Context where
@@ -46,7 +65,9 @@ instance API Context where
 -- manager across all of your API calls.
 defaultContext :: Key -> IO Context
 defaultContext key = do dir <- defaultDir
-                        return $ Context key "en" dir Nothing
+                        return $ Context key defLang dir defTO Nothing
+  where defLang = "en"
+        defTO   = Just 60000000
 
 -- Base URL for the service.
 apiBaseURL :: URL
@@ -67,4 +88,18 @@ makeURL c q  = T.concat [apiBaseURL, path q (apiKey c) (apiLang c), qStr]
 
 -- Use the HTTP module to make a remote HTTP GET call for the API.
 fetchC :: Query query => Context -> query -> Disposition r -> IO (Result r)
-fetchC c q = get (makeURL c q) (httpManager c)
+fetchC c q pipe =
+  do req <- H.parseUrl $ T.unpack $ (makeURL c q)
+     let req' = req {H.responseTimeout = httpTimeout c}
+     runWithManager (httpManager c) $ \manager -> do
+       response <- H.http req' manager
+       if HS.statusIsSuccessful $ H.responseStatus response
+         then liftM Right (H.responseBody response C.$$+- pipe)
+         else return . Left $ NetworkError errorMsg
+  where errorMsg = "the server did not respond with the requested information"
+
+-- Internal function to ensure that 'runResourceT' is called.
+runWithManager :: Maybe H.Manager -> (H.Manager -> ResourceT IO a) -> IO a
+runWithManager manager f = case manager of
+  Just m  -> runResourceT $ f m
+  Nothing -> H.withManager f
